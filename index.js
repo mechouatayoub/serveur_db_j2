3605ba93c4f7a135c354b8f910d902953241f071
Ayoub Mechouat <ayoub.mechouat@gmail.com>
2023-03-27 00:38:24 +0200

    J2 - To do list - Router creation

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const expressformidable = require("express-formidable");
 const drugstore = require("./drugstore.js");
+const todolist = require("./todolist.js");
 
 app.use(expressformidable());
 app.use("/drugstore", drugstore);
+app.use("/todolist", todolist);
 
 app.listen(3000, (req, res) => {
   console.log("Server started");
